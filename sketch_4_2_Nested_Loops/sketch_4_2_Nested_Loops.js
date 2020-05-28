ed1c60c2b5df640e95c6759f1b518f5b05134a37
Siddhant Jain <50648970+siddhantjain07@users.noreply.github.com>
2020-05-28 16:31:47 +0530

    Add files via upload

--- sketch_4_2_Nested_Loops/sketch_4_2_Nested_Loops.js
+++ sketch_4_2_Nested_Loops/sketch_4_2_Nested_Loops.js
@@ -1,3 +1,5 @@
+var on = false;
+
 function setup() {
 createCanvas(640,360);
 }
@@ -8,12 +10,24 @@ background(0);
 stroke(255);
 strokeWeight(2);
 
+if(on){
+  r = map(mouseX,0,width,0,255);
+  g = map(mouseX,0,width,255,0);
+  b = map(mouseY,0,width,0,255);
+background(r,g,b);
+}
 
 for (var x=0 ; x<=mouseX ; x += 50){
   for(var y=0 ; y<= mouseY ; y += 50){
     fill(random(255),random(255),random(255));
+    ellipseMode(CENTER);
     ellipse(x,y,25,25);
   }
 }
 
+}
+
+
+function mousePressed() {
+on = !on;
 }
