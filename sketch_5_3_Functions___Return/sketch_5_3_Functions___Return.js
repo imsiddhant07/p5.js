6021721e2b9c07e2a35ef72d4bc5e044329a3650
Siddhant Jain <50648970+siddhantjain07@users.noreply.github.com>
2020-05-29 14:12:08 +0530

    Add files via upload

---
+++ sketch_5_3_Functions___Return/sketch_5_3_Functions___Return.js
@@ -0,0 +1,15 @@
+function setup() {
+var km = milesTokm(284.3);
+print(km);
+var km2= milesTokm(15);
+print(km2);
+}
+
+function milesTokm(miles) {
+var km = miles*1.6;
+return km;
+}
+
+function draw() {
+
+}
